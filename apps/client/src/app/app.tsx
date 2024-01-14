7363489a4ed0a65aeb07befc93911aa73f9c02d7
Daniel Cohen <c0.daniel06@gmail.com>
2024-01-14 13:21:08 +0200

    feat: create button component

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import styles from './app.module.css';
+import { Button } from '../components/button';
 
 export const App: React.FC = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <p>Hello World</p>
+      <Button/>
+    </div>
+  );
 };
---
+++ apps/client/src/components/button.tsx
@@ -0,0 +1,5 @@
+
+
+export const Button = () => {
+    return <button type="button">Click Me!</button>;
+}
