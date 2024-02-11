789e036c72b95a9e7a1fd8c52d1ea0fe0b81e2cd
Daniel Cohen <c0.daniel06@gmail.com>
2024-02-11 12:00:56 +0200

    feat: css adding, using tailwind to youtube icon and sign in

--- apps/client/src/features/navbar/navbar.tsx
+++ apps/client/src/features/navbar/navbar.tsx
@@ -1,19 +1,29 @@
 import { SignedIn, UserButton, SignedOut } from '@clerk/clerk-react';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaYoutube } from 'react-icons/fa';
+import { FaUser } from 'react-icons/fa6';
 
 export const Navbar: React.FC = () => {
   return (
-    <div className="w-full h-fit p-4 border-b border-gray-300 flex gap-4 justify-between items-center">
-      <Link to={'/'}>youtube</Link>
-        <SignedIn>
-          <UserButton afterSignOutUrl="/sign-in" />
-        </SignedIn>
-        <SignedOut>
-      <Link to={'/sign-in'} className="hover:bg-gray-200 rounded-lg px-4 py-2">
+    <div className="flex h-fit w-full items-center justify-between gap-4 border-b border-gray-300 p-4">
+      <div className=" flex items-center justify-between gap-1 text-2xl sm:text-3xl lg:text-4xl">
+        <FaYoutube />
+        <Link to={'/'}>
+          You<span className="font-bold">tube</span>
+        </Link>
+      </div>
+      <SignedIn>
+        <UserButton afterSignOutUrl="/sign-in" />
+      </SignedIn>
+      <SignedOut>
+        <Link
+          to={'/sign-in'}
+          className=" flex items-center justify-between gap-2 rounded-lg px-4 py-2 hover:bg-gray-200 ">
+          <FaUser />
           Sign In
-      </Link>
-          </SignedOut>
+        </Link>
+      </SignedOut>
     </div>
   );
 };
