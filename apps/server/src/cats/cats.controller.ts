3934084c586d65c4104368b1a3123bccfb4b1870
Daniel Cohen <c0.daniel06@gmail.com>
2024-02-25 16:09:59 +0200

    feat: docs nestjs

--- apps/server/src/cats/cats.controller.ts
+++ apps/server/src/cats/cats.controller.ts
@@ -1,9 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Req } from '@nestjs/common';
+import { Request } from 'express';
+import { CreateCatDto } from './dto/create-cat.dto';
 
 @Controller('cats')
 export class CatsController {
   @Get()
   findAll(): string {
     return 'This action returns all cats';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    return `This action returns a #${id} cat`;
+  }
+
+  @Post()
+  async create(@Body() createCatDto: CreateCatDto) {
+    return createCatDto;
   }
 }
