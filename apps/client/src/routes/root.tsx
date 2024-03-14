4439d213293ab888d90a6cb71a908b39be83bcb5
Daniel Cohen <c0.daniel06@gmail.com>
2024-03-14 11:54:08 +0200

    feat(users): end to end authentication

--- apps/client/src/routes/root.tsx
+++ apps/client/src/routes/root.tsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Navbar } from '../features/navbar';
+import { useUser } from '@clerk/clerk-react';
+import { useFindOrCreateMutation } from '../store/services/users.api';
 
 export const Root: React.FC = () => {
+  const { user } = useUser();
+
+  if (!user) {
+    throw new Error('You have to log in');
+  }
+
+  const [findOrCreate] = useFindOrCreateMutation({
+    fixedCacheKey: 'logged-in-user',
+  });
+
+  useEffect(() => {
+    findOrCreate({ clerkId: user.id });
+  }, [findOrCreate, user.id]);
+
   return (
     <div className="flex h-full w-full flex-col bg-white">
       <Navbar />
--- apps/client/src/store/services/users.api.ts
+++ apps/client/src/store/services/users.api.ts
@@ -1,7 +1,12 @@
+import { users, Prisma } from '@prisma/client';
 import { serverApi } from './server.api';
 
 export const usersApi = serverApi.injectEndpoints({
   endpoints: (builder) => ({
-    findOrCreate: builder.mutation({ query: () => 'find-or-create' }),
+    findOrCreate: builder.mutation<users, Prisma.usersCreateInput>({
+      query: (body) => ({ url: 'users/find-or-create', body, method: 'POST' }),
+    }),
   }),
 });
+
+export const { useFindOrCreateMutation } = usersApi;
